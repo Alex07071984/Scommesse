12ca7d0d8f3ec760f738209190fa9b05cee59897
penn_ <penn_@PcAlessandro>
2022-04-21 12:07:59 +0200

    # WARNING: head commit changed in the meantime
    Nuovo Progetto

---
+++ Scommesse/src/main/java/com/ProgettoScommesse/Scommesse/ScommesseApplication.java
@@ -0,0 +1,13 @@
+package com.ProgettoScommesse.Scommesse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScommesseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScommesseApplication.class, args);
+	}
+
+}
